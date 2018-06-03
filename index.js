a0b30c4073a516cd1062e3ce5e129376df533a7c
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-03 17:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten (name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten (name){
+  kittens.unshift(name)
+}
+function destrucivelyRemoveLastKitten(){
+  kittens.slice(-1)
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.slice(0)
+}
+function appendKitten(name){
+  var kittens1 = [...kittens,name]
+  return kittens1
+}
+function prependKitten(name){
+  var kittens1 = [name,...kittens]
+  return kittens1
+}
+function removeLastKitten(){
+  var kittens1 = kittens.splice(0,kittens.length-1)
+  return kittens1
+}
